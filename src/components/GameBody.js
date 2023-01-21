8f56eb8bda5d5f16bf03d7bd97a9d632e6597c76
bst003 <bstanner33@yahoo.com>
2023-01-21 13:53:17 -0500

    Begin work on fetch from Poke api

--- src/components/GameBody.js
+++ src/components/GameBody.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function GameBody(props) {
+    const getPokemon = async () => {
+        try {
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=12");
+
+            const pokemonData = await response.json();
+
+            console.log(pokemonData);
+        } catch (err) {
+            console.log("this is an error");
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getPokemon();
+    }, []);
+
     return (
         <main>
             <section className="">
