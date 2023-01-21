0b5d6e7fd1008149ea6ae660c644e8e6852f3df0
bst003 <bstanner33@yahoo.com>
2023-01-21 14:00:20 -0500

    Update notes for GameBody component

--- src/components/GameBody.js
+++ src/components/GameBody.js
@@ -1,9 +1,20 @@
 import React, { useEffect } from "react";
 
+/*
+
+How to set up fetches for individual pokemon data?
+    - Does this have to be set up on the top level (GameBody)
+      or can it be set up on lower level?
+    - Consider how re-rendering will occur. Can't perform fetch each re-render
+    - Pass pokemon ID to sub compoent and only run fetch if ID changes
+
+*/
+
 function GameBody(props) {
     const getPokemon = async () => {
         try {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=12");
+            // const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=12");
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon/1");
 
             const pokemonData = await response.json();
 
