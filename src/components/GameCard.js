a78379b65cc6afca26877d3493377e0c104b9d9f
bst003 <bstanner33@yahoo.com>
2023-01-25 23:47:35 -0500

    Style site and begin setup of clicked methods

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,10 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+/*
+
+How to undo clicked on game over?
+
+
+*/
 
 function GameCard(props) {
     const { name, sprite } = props;
 
+    const [clicked, setClicked] = useState(0);
+
+    const triggerButton = () => {
+        if (!clicked) {
+            console.log("the button is clicked");
+            setClicked(1);
+            return;
+        }
+
+        console.log("game over");
+    };
+
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        const buttonElement = buttonRef.current;
+
+        buttonElement.addEventListener("click", triggerButton);
+
+        return () => {
+            buttonElement.removeEventListener("click", triggerButton);
+        };
+    });
+
     return (
-        <button className="gameCard" type="button">
+        <button className="gameCard" type="button" ref={buttonRef}>
             <div className="gameCard__inner">
                 <img src={sprite} alt="Sprite of {name}" />
                 <p>{name}</p>
