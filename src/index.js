d93a20dabfc25dd65a1ffa6a98394d3c1c8f1fe5
bst003 <bstanner33@yahoo.com>
2023-01-27 16:35:18 -0500

    Remove un-needed comments in index

--- src/index.js
+++ src/index.js
@@ -11,31 +11,3 @@ root.render(
     <App />
     // </React.StrictMode>
 );
-
-/*
-Thoughts
-  - Should Body State hold "clicked" state for individual cards? 
-  - Will use pokeAPI
-
-Structure
-  - App.js
-    * Functionality
-      * Update score method
-      * Method to check if score is higher than high score
-      * State
-        * Score
-        * High Score
-    - Header.js (component)
-      - Title (html element)
-      - Scoreboard.js (component)
-    - Body.js (component)
-      * Functionality
-        * Performs API Call
-          * on useEffect(() => {}, []); (componentDidMount) perform fetch of data from API
-          * format: "https://pokeapi.co/api/v2/pokemon/?limit=12&offset=20" 
-        * Array to hold cards
-        * Method to shuffle cards
-      - Instructions (html element)
-      - Card.js (component)
-
-*/
